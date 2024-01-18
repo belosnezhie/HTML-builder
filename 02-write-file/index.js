3129c7c6329f11262eeed481582d8f95180dfb0e
Maria Treier <mariatraier@gmail.com>
2024-01-18 22:54:04 +0100

    feat: complete 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join('./02-write-file', 'text.txt');
+
+const writableStream = fs.createWriteStream(filePath, 'utf-8');
+
+stdout.write('Hellow! Please put smth to the console.\n');
+
+stdin.on('data', (data) => {
+  if (data.toString() === 'exit\n') {
+    process.exit();
+  }
+  writableStream.write(data);
+});
+
+process.on('exit', () => {
+  stdout.write('Bye!\n');
+});
+
+process.on('SIGINT', () => {
+  stdout.write('\n');
+  process.exit();
+});
