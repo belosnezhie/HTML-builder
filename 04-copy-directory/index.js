dfb0772f6c6788bd6a36cd14363992e3cb85dfae
Maria Treier <mariatraier@gmail.com>
2024-01-20 22:57:52 +0100

    feat: implement copyDirectory function in 06-build-page task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -28,26 +28,3 @@ fs.rm(newPath, { recursive: true }, () => {
     });
   });
 });
-
-// fs.mkdir(newPath, { recursive: true }, (err) => {
-//   if (err) {
-//     return console.error(err);
-//   }
-//   fs.readdir(dirPath, { withFileTypes: true }, (err, dirents) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       dirents.forEach((file) => {
-//         fs.copyFile(
-//           path.join(file.path, file.name), // from
-//           path.join(newPath, file.name), // to
-//           (err) => {
-//             if (err) {
-//               console.error(err);
-//             }
-//           },
-//         );
-//       });
-//     }
-//   });
-// });
---
+++ 06-build-page/index.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+
+// Folder project-dist
+const distPath = path.join(__dirname, 'project-dist');
+// HTML
+const pagePath = path.join(__dirname, 'project-dist', 'index.html');
+// CSS
+const stylesPath = path.join(__dirname, 'styles');
+const newStylePath = path.join(__dirname, 'project-dist', 'style.css');
+// Assets
+const assetsPath = path.join(__dirname, 'assets');
+const newAssetsPath = path.join(__dirname, 'project-dist', 'assets');
+// Templates
+const templPath = path.join(__dirname, 'project-dist', 'template.html');
+
+// const writableStreamHTML = fs.createWriteStream(distPath, 'utf-8');
+
+function copyDirectory(oldPath, newPath) {
+  fs.rm(newPath, { recursive: true }, () => {
+    fs.mkdir(newPath, { recursive: true }, (err) => {
+      if (err) {
+        return console.error(err);
+      }
+      fs.readdir(oldPath, { withFileTypes: true }, (err, dirents) => {
+        if (err) {
+          console.log(err);
+        } else {
+          dirents.forEach((entry) => {
+            if (!entry.isFile()) {
+              return copyDirectory(
+                path.join(entry.path, entry.name),
+                path.join(newPath, entry.name),
+              );
+            }
+            fs.copyFile(
+              path.join(entry.path, entry.name), // from
+              path.join(newPath, entry.name), // to
+              (err) => {
+                if (err) {
+                  console.error(err);
+                }
+              },
+            );
+          });
+        }
+      });
+    });
+  });
+}
+
+function copyAssets() {
+  return copyDirectory(assetsPath, newAssetsPath);
+}
+
+copyAssets();
+// copyStyles();
+// copyTemplate();
+// tranformTemplate();
