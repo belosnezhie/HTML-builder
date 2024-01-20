452d1861054d57d60a9be2d97b2d2e972819730f
Maria Treier <mariatraier@gmail.com>
2024-01-20 20:51:35 +0100

    feat: add deliting file copy folder logic to 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,25 +4,50 @@ const path = require('path');
 const dirPath = path.join(__dirname, 'files');
 const newPath = path.join(__dirname, 'files-copy');
 
-fs.mkdir(newPath, { recursive: true }, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  fs.readdir(dirPath, { withFileTypes: true }, (err, dirents) => {
+fs.rm(newPath, { recursive: true }, () => {
+  fs.mkdir(newPath, { recursive: true }, (err) => {
     if (err) {
-      console.log(err);
-    } else {
-      dirents.forEach((file) => {
-        fs.copyFile(
-          path.join(file.path, file.name), // from
-          path.join(newPath, file.name), // to
-          (err) => {
-            if (err) {
-              console.error(err);
-            }
-          },
-        );
-      });
+      return console.error(err);
     }
+    fs.readdir(dirPath, { withFileTypes: true }, (err, dirents) => {
+      if (err) {
+        console.log(err);
+      } else {
+        dirents.forEach((file) => {
+          fs.copyFile(
+            path.join(file.path, file.name), // from
+            path.join(newPath, file.name), // to
+            (err) => {
+              if (err) {
+                console.error(err);
+              }
+            },
+          );
+        });
+      }
+    });
   });
 });
+
+// fs.mkdir(newPath, { recursive: true }, (err) => {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   fs.readdir(dirPath, { withFileTypes: true }, (err, dirents) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       dirents.forEach((file) => {
+//         fs.copyFile(
+//           path.join(file.path, file.name), // from
+//           path.join(newPath, file.name), // to
+//           (err) => {
+//             if (err) {
+//               console.error(err);
+//             }
+//           },
+//         );
+//       });
+//     }
+//   });
+// });
